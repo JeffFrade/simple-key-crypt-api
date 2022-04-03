5f4b11e520dd441a85d66c11de6ebf0f7b51ea5c
Jefferson Frade <jefferrson.frade@gmail.com>
2022-04-03 01:08:48 -0300

    Finished Crypt, Decrypt and Alphabet

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/crypt', 'CryptController@crypt')->name('crypt');
+Route::post('/decrypt', 'CryptController@decrypt')->name('decrypt');
+Route::post('/alphabet', 'CryptController@alphabet')->name('alphabet');
