4ab352b8da29e863a43b5688fdf40b90d6ebb18c
Jefferson Frade <jefferrson.frade@gmail.com>
2022-04-03 00:30:30 -0300

    Refactored Providers

--- config/app.php
+++ config/app.php
@@ -169,8 +169,8 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        App\Core\Providers\AppServiceProvider::class,
+        App\Core\Providers\RouteServiceProvider::class,
 
     ],
 
